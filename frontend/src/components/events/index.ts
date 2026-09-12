3ad07ca219c1e7a4da97a1ef2c1ff2ea3819a7a0
Craig <craig@example.com>
2026-09-12 08:54:28 +0100

    chore: remove legacy event filters

--- frontend/src/components/events/index.ts
+++ frontend/src/components/events/index.ts
@@ -1,6 +1,5 @@
 export { EventList } from './EventList';
 export { EventCard } from './EventCard';
-export { EventFilters } from './EventFilters';
 export { DateFilterPills } from './DateFilterPills';
 export { CategoryFilterPills } from './CategoryFilterPills';
 
